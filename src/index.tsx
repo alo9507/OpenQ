f02647ec1c42339738d241ab99d0354d1a932ccd
Andrew <alo42@georgetown.edu>
2020-03-18 12:42:07 -0400

    added authentication to Apollo Client

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,28 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { setContext } from "apollo-link-context";
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
+const httpLink = new HttpLink({
   uri: "https://api.github.com/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = "ef778fd58293e0fbce5b3e96c5b01070fe16b5c0";
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`
+    }
+  };
 });
 
 const client = new ApolloClient({
   cache,
-  link
+  link: authLink.concat(httpLink)
 });
 
 ReactDOM.render(
