98a380f3793f5e5c2171d7bd6c951a3473bbffe4
Andrew <alo42@georgetown.edu>
2020-03-18 12:10:07 -0400

    added Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Issues from "./Issues";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{window.location.pathname}</p>
+        <Issues />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Issues.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Issues extends Component {
+  render() {
+    return <div>Github Issues Here</div>;
+  }
+}
+
+// {
+//   github {
+//     repo(name: "diez", ownerUsername:"diez") {
+//       issues {
+//         title
+//       }
+//     }
+//   }
+// }
