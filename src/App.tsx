34d4be9bedc98482e7bf92117ff050487b3f9721
Andrew <alo42@georgetown.edu>
2020-03-19 17:50:02 -0400

    added issue incrementer

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,7 @@ import Issues from "./Issues";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{window.location.pathname}</p>
-        <Issues />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Issues />
     </div>
   );
 }
--- src/Issues.tsx
+++ src/Issues.tsx
@@ -1,32 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
+let count = 0;
+
 function Issues() {
+  const [count, setCount] = useState(0);
+
   const { data, loading, error } = useQuery(GET_ISSUES);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
 
-  return <div>{data}</div>;
+  const issues = data.repository.issues.edges;
+
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Get Story</button>
+      <div>{issues[count].node.title}</div>
+    </>
+  );
 }
 
 export default Issues;
 
 const GET_ISSUES = gql`
-  {
-    query {
-      repository(owner: "octocat", name: "Hello-World") {
-        issues(last: 20, states: CLOSED) {
-          edges {
-            node {
-              title
-              url
-              labels(first: 5) {
-                edges {
-                  node {
-                    name
-                  }
+  query {
+    repository(owner: "diez", name: "diez") {
+      issues(last: 20, states: CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first: 5) {
+              edges {
+                node {
+                  name
                 }
               }
             }
