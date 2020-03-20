6e02b052a91f3dd385744b2c72483a37cdc812df
Andrew <alo42@georgetown.edu>
2020-03-19 22:15:00 -0400

    adds a fresh screen for when Get Story has yet to be pressed

--- src/components/Issues.tsx
+++ src/components/Issues.tsx
@@ -14,22 +14,33 @@ function Issues(props: any) {
   const { pq, openIssuesCount } = props;
 
   const [currentIssue, setCurrentIssue] = useState<Issue | null>(null);
+  const [fresh, setFresh] = useState(true);
+
   console.log(pq);
 
   return (
     <>
       <h1>There are {openIssuesCount} open issues that could use your help!</h1>
-      <GetStoryButton callback={() => setCurrentIssue(pq.pop())} />
+      <GetStoryButton callback={() => getStoryPressed()} />
       <br />
 
       {/* If it hasn't been clicked yet then don't render the IssueDescription */}
       {currentIssue == null ? (
-        <h1>No More Stories</h1>
+        !fresh ? (
+          <h1>No More Stories</h1>
+        ) : (
+          <h3>Click to get story</h3>
+        )
       ) : (
         <IssueDescription issue={currentIssue} />
       )}
     </>
   );
+
+  function getStoryPressed() {
+    setFresh(false);
+    setCurrentIssue(pq.pop());
+  }
 }
 
 export default Issues;
